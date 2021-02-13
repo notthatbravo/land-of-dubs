3b7c663e0ac79effd2effc07c211baff0b6fa642
Bravo <adeolaisrael424@gmail.com>
2021-02-14 00:51:42 +0100

    Initial Commit: Login page

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+let menuButton = document.querySelector(".hamburger-icon");
+let closeButton = document.querySelector(".close-icon");
+let mobileNavMenu = document.querySelector(".desktop-nav");
+
+menuButton.addEventListener("click", toggleNav);
+closeButton.addEventListener("click", hideNav);
+
+function toggleNav() {
+    mobileNavMenu.style.height = "15em";
+    menuButton.style.display = "none";
+    closeButton.style.display = "block";
+}
+
+function hideNav() {
+    mobileNavMenu.style.height = "0";
+    menuButton.style.display = "block";
+    closeButton.style.display = "none";
+}
