359cd46c6f61c45d3ecb0663ebab2118e8bab8ad
Bravo <adeolaisrael424@gmail.com>
2021-02-20 17:22:21 +0100

    initial Commit: Forgot password page

--- assets/js/login-signup.js
+++ assets/js/login-signup.js
@@ -5,13 +5,16 @@ let passwordInput = document.getElementsByClassName("password-input"); // input 
 eye.addEventListener('click', togglePassword)
 
 function togglePassword() {
-    for (var i=0; i < passwordInput.length; i++) { // loop affects the input field in login and signup page
-        passwordInput[i].setAttribute("type", "text"); // changes type = password to type = text
-    }
     eye.classList.toggle("fa-eye-slash"); // toggles the eye icon state
 
-    if (hasClass(eye, "fa-eye-slash")) {
-        passwordInput.setAttribute("type", "password");
+    for (var i=0; i < passwordInput.length; i++) { // loop affects the input field in login and signup page
+        passwordInput[i].setAttribute("type", "text"); // changes type = password to type = text
+    };
+
+    if (eye.classList.contains("fa-eye-slash")) {
+        for (var i=0; i < passwordInput.length; i++) {
+            passwordInput[i].setAttribute("type", "password");
+        }
     }; // this if block doesn't work
     // motive: if eye icon slashed (meaning it's currently showing the password), when it's clicked,
     // change the type = text back to type = password
